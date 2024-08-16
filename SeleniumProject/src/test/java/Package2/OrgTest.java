3248d309e84801090d92272b64c4c544ce33a672
Gopikamano24 <gopikamano35@gmail.com>
2024-08-16 16:41:14 +0530

    second commit

--- SeleniumProject/src/test/java/Package2/OrgTest.java
+++ SeleniumProject/src/test/java/Package2/OrgTest.java
@@ -12,5 +12,10 @@ public class OrgTest {
 		System.out.println("execute modifyOrgTest ");
 		
 	}
+	@Test
+	public void deleteOrgTest() {
+		System.out.println("execute deleteOrgTest ");
+		
+	}
 	
 }
