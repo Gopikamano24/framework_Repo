2fe4fdfac1850f72fc0493c22a37b8fc3d315559
Gopikamano24 <gopikamano35@gmail.com>
2024-08-16 15:38:11 +0530

    first commit

---
+++ SeleniumProject/src/test/java/Package1/ContactTest.java
@@ -0,0 +1,16 @@
+package Package1;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createcontactTest() {
+		System.out.println("execute createcontactTest ");
+	}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute modifycontactTest ");
+		
+	}
+	
+}
